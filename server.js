599c2f4a8c7275c497a29e17626d0edf4ea4130b
himonrana <himon.rana@chasacademy.se>
2018-11-02 14:33:45 +0100

    try websocket

--- server.js
+++ server.js
@@ -1,63 +1,25 @@
-const mongo = require('mongodb').MongoClient;
-const client = require('socket.io').listen(4000).sockets;
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
-// connet to mongo
-mongo.connect('mongodb://127.0.0.1/buntchat', { useNewUrlParser: true }, function (err, database) {
-    if (err) {
-        throw err;
-    }
-    let db = database.db('buntchat');
+port = 4000
+server.listen(port);
+console.log("Listening on port: " + port);
+// WARNING: app.listen(80) will NOT work here!
 
-    console.log('MongDB connected...')
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
 
-    // Connect to Socket.io
-    client.on('connection', function (socket) {
+io.on('connection', function (socket) {
+  io.emit('this', { will: 'be received by everyone' });
+  socket.emit('status', socket);
 
-        let chat = db.collection('chats');
-        // Create function to send status
-        sendStatus = function (s) {
-            socket.emit('status', s);
-        }
+  socket.on('private message', function (from, msg) {
+    console.log('I received a private message by ', from, ' saying ', msg);
+  });
 
-        // Get chats from mongo collection
-        chat.find().limit(100).sort({ _id: 1 }).toArray(function (err, res) {
-            if (err) {
-                throw err;
-            }
-
-            // Emit the messages
-            socket.emit('output', res);
-        });
-
-        // Hnadle input events
-        socket.on('input', function (data) {
-            let name = data.name;
-            let message = data.message;
-
-            // Check for name and message(if empty)
-            if (name == '' || message == '') {
-                sendStatus('Please enter a name and message');
-            } else {
-                // insert message
-                chat.insert({ name: name, message: message }, function () {
-                    client.emit('output', [data]);
-
-                    // Sen status object
-                    sendStatus({
-                        message: 'Message sent',
-                        clear: true
-                    });
-                });
-            }
-        });
-
-        // Handle clear
-        socket.on('clear', function (data) {
-            // Remove all cahts from collection
-            chat.remove({}, function () {
-                // Emit cleared
-                socket.emit('cleared');
-            });
-        });
-    });
+  socket.on('disconnect', function () {
+    io.emit('user disconnected');
+  });
 });
